69c94ab2018d0b6c2f3fa0d5314a7c2cdd1bac3e
web@poliweb.su <web@poliweb.su>
2021-04-10 14:51:40 +0600

    deploymnet mission

---
+++ graphql/queries.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+
+export const allArticlesQuery = gql `
+query allArticlesQuery {
+  articles {
+    id
+    body
+    title
+    date
+    description
+    slug
+  }
+},
+`;
+export const singleArticleQuery = gql `
+query singleArticleQuery($id: ID!) {
+  article(id: $id) {
+    id
+    body
+    title
+    date
+    description
+    slug
+  }
+},
+`;
